80fb720743b16ab83c35177629e54b8ac108190b
vncorrea <vinicorreag2001@gmail.com>
2025-06-19 20:02:28 -0300

    fix: auth controller

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { login, register } from '../controllers/auth.controller';
+import { login, register, logout } from '../controllers/auth.controller';
 
 const authRoutes = Router();
 
 authRoutes.post('/login', login);
 authRoutes.post('/register', register);
+authRoutes.post('/logout', logout);
 
 export default authRoutes;
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -28,7 +28,15 @@ export async function register({
   email: string;
   password: string;
 }) {
+  const userExists = await prisma.user.findUnique({ where: { email } });
+  
+  if (userExists) throw new Error("Usuário já existe");
+
   const hash = await bcrypt.hash(password, 10);
   const user = await prisma.user.create({ data: { name, email, password: hash } });
   return { id: user.id, name: user.name, email: user.email };
+}
+
+export async function logout(id: string) {
+  return { success: true };
 }
