90456bea86fb8da3900ddf6253e476656db6d413
vncorrea <vinicorreag2001@gmail.com>
2025-06-19 20:08:44 -0300

    fix: logout

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -38,12 +38,5 @@ export async function register(req: Request, res: Response) {
 }
 
 export async function logout(req: Request, res: Response) {
-  try {
-    const { id } = req.body;
-    await AuthService.logout(id);
-    res.status(200).json({ message: "Logout realizado com sucesso" });
-  } catch (error) {
-    res.status(400).json({ message: (error as Error).message });
-    return;
-  }
+  res.status(200).json({ message: "Logout realizado com sucesso" });
 }
