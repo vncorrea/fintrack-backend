5d80e0a8cbb18ec58fb7db80e6b6d8c6e7a45227
vncorrea <vinicorreag2001@gmail.com>
2025-06-19 20:02:55 -0300

    fix: auth controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -3,14 +3,47 @@ import * as AuthService from "../services/auth.service";
 
 export async function login(req: Request, res: Response) {
   const { email, password } = req.body;
-  const user = await AuthService.login({ email, password });
 
-  res.status(200).json(user);
+  if (!email || !password) {
+    res.status(400).json({ message: "Todos os campos são obrigatórios" });
+    return;
+  }
+
+  try {
+    const user = await AuthService.login({ email, password });
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({ message: (error as Error).message });
+    return;
+  }
 }
 
 export async function register(req: Request, res: Response) {
   const { name, email, password } = req.body;
-  const user = await AuthService.register({ name, email, password });
 
-  res.status(201).json(user);
+  if (!name || !email || !password) {
+    res.status(400).json({ message: "Todos os campos são obrigatórios" });
+    return;
+  }
+
+  try {
+    const user = await AuthService.register({ name, email, password });
+
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(400).json({ message: (error as Error).message });
+    return;
+  }
+}
+
+export async function logout(req: Request, res: Response) {
+  try {
+    const { id } = req.body;
+    await AuthService.logout(id);
+    res.status(200).json({ message: "Logout realizado com sucesso" });
+  } catch (error) {
+    res.status(400).json({ message: (error as Error).message });
+    return;
+  }
 }
